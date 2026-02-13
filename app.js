b1f818fdf7b77da91fd25d33768223072475acd2
jihanejador <jihanejador@gmail.com>
2026-02-13 10:12:27 +0100

    creation le fichier app.js pour appler les fonctions

---
+++ app.js
@@ -0,0 +1,11 @@
+import { initTodo } from "./todo.js";
+import { initTimer } from "./timer.js";
+import { renderTasks } from "./ui.js";
+import { getTodos } from "./storage.js";
+
+const todos = getTodos();
+
+// renderTasks(todos);
+initTodo(todos);
+
+
